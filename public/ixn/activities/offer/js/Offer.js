02f00d6ae7a4e0b88452534ed71604f7f8c87e28
brgfighter <30729614+brgfighter@users.noreply.github.com>
2017-12-18 19:17:02 +0900

    Update Offer.js

--- public/ixn/activities/offer/js/Offer.js
+++ public/ixn/activities/offer/js/Offer.js
@@ -173,17 +173,6 @@ define( function( require ) {
 	    
 	    var webclient = require("request");
  
-webclient.get({
-  url: "https://master.laborot.com/api/push",
-  qs: {
-    uid: "U4c6cc96c2bf1ec1e54894b172b45c537",
-    scenarioid: "22",
-    test: "1"
-  }
-}, function (error, response, body) {
-  console.log(body);
-});
-    }; 
-    	 
+
 });
 			
