59a072d53d96172c0ff84f809ec1242f0f04c9fd
brgfighter <30729614+brgfighter@users.noreply.github.com>
2017-12-18 14:06:29 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,17 @@ var activityOffer = require('./activityOffer');
  * GET home page.
  */
 exports.index = function(req, res){
-    if( !req.session.token ) {
-        res.render( 'index', {
-            title: 'Unauthenticated',
-            errorMessage: 'This app may only be loaded via the Salesforce Marketing Cloud',
-        });
-    } else {
+//    if( !req.session.token ) {
+//        res.render( 'index', {
+//            title: 'Unauthenticated',
+//            errorMessage: 'This app may only be loaded via the Salesforce Marketing Cloud',
+//        });
+//    } else {
         res.render( 'index', {
             title: 'Journey Builder Activity Example',
             results: activityUtils.logExecuteData,
         });
-    }
+//    }
 };
 
 exports.login = function( req, res ) {
